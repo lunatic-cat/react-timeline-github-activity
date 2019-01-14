8970e66e01aefa0aa10ebcc85a79191527d4475d
Vladimir Sidorenko <vulkanfry@lunatic.cat>
2019-01-15 03:23:06 +0600

    Hardcode mine github public activity

--- src/App.js
+++ src/App.js
@@ -2,11 +2,45 @@ import React, { Component } from 'react';
 import './App.css';
 import 'react-vertical-timeline-component/style.min.css';
 import Timeline from './components/Timeline'
+import fetch from 'cross-fetch';
+import Loader from 'react-loader-spinner'
+import LoadingOverlay from 'react-loading-overlay';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      response: null,
+      fetched: false,
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  fetchData() {
+    fetch('//api.github.com/users/ByJIKaHkaz/events')
+      .then(res => {
+        if (res.status >= 400) {
+          throw new Error("Bad response from server");
+        }
+        return res.json();
+      })
+      .then(body => this.setState({ response: body, fetched: true }))
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
   render() {
+    const { response, fetched } = this.state;
     return (
-      <Timeline/>
+      <LoadingOverlay active={!fetched} spinner={<Loader type="Puff" color="#aaaaaa" height={80} width={80} />}>
+        {<Timeline response={response} />}
+      </LoadingOverlay>
     );
   }
 }
--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -1,32 +1,35 @@
 import React from 'react';
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import './index.css';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
+import moment from 'moment';
 
-export function Lines ({ lines } ) {
-  return lines.map((cells, index) =>
+export function Lines({ lines }) {
+  return lines.map((line, index) =>
     <VerticalTimelineElement
       className="vertical-timeline-element--work"
       key={index}
-      date="2011 - present" 
-      cells={cells}>
-        <h3 className="vertical-timeline-element-title">Creative Director</h3>
-        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-        <p>
-          Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-        </p>
+      date={moment(line.created_at).format("dddd, MMMM Do YYYY")}
+      icon={<img src={line.actor.avatar_url} alt="" className='avatar'></img>}
+      line={line}>
+      <h3 className="vertical-timeline-element-title">{line.actor.display_login}</h3>
+      <h4 className="vertical-timeline-element-subtitle">{line.type}</h4>
+      <p><a href={line.repo.url}>{line.repo.name}</a></p>
     </VerticalTimelineElement>
   );
 }
 
 class Timeline extends React.Component {
 
-
-
   render() {
+    const { response } = this.props;
+    console.log(response)
     return (
-      <VerticalTimeline>
-        <Lines lines={['A', 'B']}/>
-    </VerticalTimeline>
+      response
+        ? <VerticalTimeline>
+          <Lines lines={response} />
+        </VerticalTimeline>
+        : null
     );
   }
 }
